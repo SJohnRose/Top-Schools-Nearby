c815a08061c821ba72db74d54e4e546e212502f2
SJohnRose <shamini.johnrose@gmail.com>
2023-03-24 21:13:22 +1100

    Added code to fetch nearby schools data for radius 5000

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,66 @@
+
 var searchButton = document.getElementById('search-btn');
 var cityInputEl = document.getElementById("search-input");
-const historySectionEl = document.getElementById("search-history-section");
 const listSchoolsDivEl = document.getElementById("list-schools");
 
 
-// Function to get weather data from Open Weather Map API
+// Function to get data from Google Maps API
 function getApi() {
-    var suburbName = cityInputEl.value;
-    var requestUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAk91w3HUMHmGr0LsXGiLAif5ZqM6vofjU&libraries=places";
-    fetch(requestUrl)
-        .then((response) => response.json())
-        .then((data) => { populateData(data);});
+    initMap();
 }
 
 // Event listener for the Search button
 searchButton.addEventListener('click', getApi);
 
-// Setting the values into HTML elements
-// function setValueToElement(id, value) {
-//     document.getElementById(id).textContent = value;
-// }
+
 
 // Extracting the required temperature, wind and humidity information from weather JSON
 function populateData(data) {
     console.log(data);
-    // listSchoolsDivEl.style.visibility = "visible";
-    // var date = formatDate(String(data.list[0].dt_txt).substring(0,10));
-    // var cityDate= data.city.name + "   (" + date + ")";
-    // setValueToElement('city-name', cityDate);
-    // document.getElementById('weather-icon').src="http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
-    // setValueToElement('city-temp-1', "Temp:  " + data.list[0].main.temp + '  \u00B0' + "C");
-    // setValueToElement('city-wind-1', "Wind:  " + data.list[0].wind.speed + " MPH");
-    // setValueToElement('city-humidity-1', "Humidity:  " + data.list[0].main.humidity + " %");
-    // showFiveDayWeather(data);
     
 }
 
+// Function to initialise map
+function initMap() {
+    var locationName = cityInputEl.value;
+    getLocationData(locationName);
+    
+}
+  
+// Get place information using location name
+function getLocationData(locationName) {
+  //console.log("called");
+  var currentLocation = new google.maps.LatLng(0,0);
+  mapElement = document.getElementById('map');
+  map = new google.maps.Map(mapElement, {center: currentLocation, zoom: 10});
+  var service = new google.maps.places.PlacesService(map);
+  var request = {
+    query: locationName,
+    fields: ['name', 'geometry']
 
-// Display weather for next five days
-// function showFiveDayWeather(data) {
-//     fiveDayDivEl.style.visibility = "visible";
-//     for (var i=1; i<=5;i++) {
-//         var cityDate= data.list[7*i+3].dt_txt;
-//         setValueToElement('date'+i, formatDate(String(cityDate).substring(0,10)));
-//         document.getElementById('weather-icon-day'+i).src="http://openweathermap.org/img/wn/" + data.list[7*i+3].weather[0].icon + ".png";
-//         setValueToElement('temp-day'+i, "Temp:  " + data.list[7*i+3].main.temp + '  \u00B0' + "C");
-//         setValueToElement('wind-day'+i, "Wind:  " + data.list[7*i+3].wind.speed + " MPH");
-//         setValueToElement('humidity-day'+i, "Humidity:  " + data.list[7*i+3].main.humidity + " %");
-//     }
-// }
+  };
+  service.findPlaceFromQuery(request, getSchoolsNearby);
+}
+
+// Get schools information from Google maps 
+function getSchoolsNearby(results, status) {
+  console.log("getSchools called");
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    locationResult = results[0];
+    const currentSearchLocation = {lat: locationResult.geometry.location.lat(), lng: locationResult.geometry.location.lng()};
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: locationResult.name,
+      zoom: 17,
+      mapID: "8d193001f940fde3",
+    });
+    const service = new google.maps.places.PlacesService(map);
+    searchParams = {
+      location: currentSearchLocation,
+      radius: 5000,
+      type: "school"
+    }
+    service.nearbySearch(searchParams, (results, status, pagination) => {
+      console.log(results);
+    });
+  }
+}
